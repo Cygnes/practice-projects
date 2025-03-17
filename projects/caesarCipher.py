0b08f331af0024afed1e3f287dff4863cd87008d
Alec <salgado.alecethan@proton.me>
2025-03-17 17:06:17 -0400

    Changed caesarCiphar.py to user execution

--- projects/caesarCipher.py
+++ projects/caesarCipher.py
@@ -1,8 +1,5 @@
-#! /usr/bin/env python3
 # caesarCipher.py - shift +3 cipher encoder/decoder
-
 import sys
-import pyperclip
 
 cipherText = {
     'a': 'd',
@@ -52,34 +49,31 @@ def decodeText(cipherText, processedText):
     
     return ''.join(finalText)
 
-def main():
-    if len(sys.argv) < 2: # shows the feature of the program 
-        print("Usage: caesarCipher.py - encodes/decodes text in shift +3 encryption")
-        sys.exit()
+def main(): 
+    while True: 
+        text = input("Enter text here or press Q to quit: ")
+        # Quits the encoder/decoder 
+        if text.startswith("Q"):
+            sys.exit()
+        # avoids decimal and special characters from the pasted text
+        if text.isdecimal():
+            print('Text must only have letters not numerical characters, please try again')
+            sys.exit()
 
-    functionCipher = sys.argv[1] # terminal argument if user wants to encode/decode a text 
-    text = pyperclip.paste() # pasted text to be decoded/encoded
+        user = input("Encode/Decode: (E | D) ")
 
-    # avoids decimal and special characters from the pasted text
-    if text.isdecimal():
-        print('Text must only have letters not numerical characters, please try again')
-        sys.exit()
+        # splits the text to be decode/encoded
+        processedText = list(text.lower())
 
-    # splits the text to be decode/encoded
-    processedText = list(text)
+        if user == "E": # encodes the pasted text
+            text = encodeText(cipherText, processedText)
+        elif user == "D": # decodes the pasted text
+            text = decodeText(cipherText, processedText) 
+        else:
+            print("Error: command does not exist")
+            sys.exit()
 
-
-    if functionCipher == "encode": # encodes the pasted text
-        text = encodeText(cipherText, processedText)
-        print("Encrypted text copied to clipboard")
-    elif functionCipher == "decode": # decodes the pasted text
-        text = decodeText(cipherText, processedText) 
-        print("Decrypted text copied to clipboard")
-    else:
-        print("Error: function argument does not exist")
-        sys.exit()
-    
-    pyperclip.copy(text)
-
+        print(f"Final Result: {text}")
+        
 if __name__ == "__main__":
     main()
